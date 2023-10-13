14c6721b11359453acf17ba155f51aceeeb1c81c
Inácio Rodrigues <inaciormgalvao@gmail.com>
2023-10-13 16:10:13 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center text-3xl text-slate-dark-1">
+      <h1>Next Template Teams</h1>
+    </main>
+  )
+}
